1be04d6a09c63c6da51338da28de6e33405bd78a
Ezabulbary <ezabul.bary@gmail.com>
2022-05-10 22:43:42 +0600

    working directory

---
+++ src/component/AddUser/AddUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h1>You Can add your user</h1>
+        </div>
+    );
+};
+
+export default AddUser;
